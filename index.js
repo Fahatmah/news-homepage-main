c26023435935bb2e9804595da4c50aeafc5cd278
fahatmah <fahatmahmabang9@gmail.com>
2022-11-08 22:12:42 +0800

    fixed data

--- index.js
+++ index.js
@@ -29,15 +29,15 @@ window.addEventListener('scroll', () => {
 const news = [
   {
     title: 'Hydrogen VS Electric Cars',
-    text: 'Will hydrogen-fueled cars ever catch up to EVs?',
+    text: `Will hydrogen-fueled cars ever catch up to EVs?`,
   },
   {
     title: 'The Downsides of AI Artistry',
-    text: 'What are the possible adverse effects of on-demand AI image generation?',
+    text: `What are the possible adverse effects of on-demand AI image generation?`,
   },
   {
     title: 'Is VC Funding Drying Up?',
-    text: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.',
+    text: `Private funding by VC firms is down 50% YOY. We take a look at what that means.`,
   },
 ]
 
@@ -60,19 +60,19 @@ const topics = [
     img: 'assets/images/image-retro-pcs.jpg',
     number: 01,
     title: 'Reviving Retro PCs',
-    text: 'What happens when old PCs are given modern upgrades?',
+    text: `What happens when old PCs are given modern upgrades?`,
   },
   {
     img: 'assets/images/image-top-laptops.jpg',
     number: 02,
     title: 'Top 10 Laptops of 2022',
-    text: 'Our best picks for various needs and budgets.',
+    text: `Our best picks for various needs and budgets.`,
   },
   {
     img: 'assets/images/image-gaming-growth.jpg',
     number: 03,
     title: 'The Growth of Gaming',
-    text: 'How the pandemic has sparked fresh opportunities.',
+    text: `How the pandemic has sparked fresh opportunities.`,
   },
 ]
 
@@ -82,7 +82,7 @@ let topicItem = topics.map((item) => {
   return `<div class="topic">
             <div class="container">
               <div class="img__container">
-                <img src=${item.img} alt=${item.title} class="img__topic">
+                <img src=${item.img} alt="" class="img__topic">
               </div>
               <div class="topic__content">
                 <p class="topic__number n-clr-200">0${item.number}</p>
